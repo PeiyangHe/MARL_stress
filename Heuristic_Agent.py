26882697122534ad21c21b147a739211f6762d59
PeiyangHe <58012335+PeiyangHe@users.noreply.github.com>
2020-03-09 10:42:29 +0000

    update

--- Heuristic_Agent.py
+++ Heuristic_Agent.py
@@ -1,16 +1,18 @@
 import numpy as np
 
 class Heuristic_Agent:
-    def __init__(self, bar=0.035, name=None):
+    def __init__(self, bar=0.035, target=0.045, percent=1, name=None):
         self.bar = bar
+        self.target = target
+        self.percent = percent
         self.name = name
 
     def act(self, state, env=None):
         agent_bank = env.allAgentBanks[self.name]
         lev = agent_bank.get_leverage_ratio()
-        if lev < self.bar:
+        if lev < self.bar + (self.target - self.bar) * self.percent:
             asset = agent_bank.get_asset_value()
-            portion_to_sell = 1 - (agent_bank.get_equity_value()/(asset*self.bar))
+            portion_to_sell = 1 - (agent_bank.get_equity_value()/(asset*self.target))
         else:
             portion_to_sell = 0
         return np.asarray([portion_to_sell, portion_to_sell])
